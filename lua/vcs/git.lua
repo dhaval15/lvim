57dd22b1ee887ced0bab7ad8a476e30cfc1d9bd7
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-09-07 01:35:38 +0000

    fix: added setup on config

--- lua/vcs/git.lua
+++ lua/vcs/git.lua
@@ -5,7 +5,11 @@ M.plugins = {
 		'TimUntersberger/neogit',
 		dependencies = {
 			{ 'nvim-lua/plenary.nvim' },
-		}
+		},
+		config = function()
+			local neogit = require('neogit')
+			neogit.setup()
+		end,
 	},
 }
 
